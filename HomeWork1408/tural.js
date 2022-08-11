9943d6f9a6e0e0c4cb8b3cd059e2ba664c7c4b1a
turalismylv <turalhi@code.edu.az>
2022-08-11 15:34:50 +0400

    HomeWork1408

--- HomeWork1408/tural.js
+++ HomeWork1408/tural.js
@@ -1,3 +1,4 @@
+//Task-2
 const myarr = [2, 4, 6, 8, 0];
 const myarr2 = [];
 
@@ -13,3 +14,22 @@ const myFunction = function (arr, arr2, index) {
 }
 
 const result = myFunction(myarr, myarr2, 3);
+
+
+//Task-1
+
+const myarrr = [1, 3, 5, 7, 9];
+const myarrr2 = [];
+
+
+const myFunction1 = function (arr, arr2, index, value) {
+    for (let i = 0; i < arr.length; i++) {
+        if (i === index) {
+            arr2.push(value)
+        }
+        arr2.push(arr[i]);
+    }
+    console.log(arr2);
+}
+
+const result1 = myFunction1(myarrr, myarrr2, 2, "SALAM");
