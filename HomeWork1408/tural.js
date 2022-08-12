7656e1ff1dd5532dc73fe335a6816af9b0183202
turalismylv <turalhi@code.edu.az>
2022-08-12 13:38:03 +0400

    HomeWork1408

--- HomeWork1408/tural.js
+++ HomeWork1408/tural.js
@@ -10,10 +10,17 @@ const myFunction = function (arr, index) {
         }
         myNewArr.push(arr[i]);
     }
-    console.log(myNewArr);
+    if(index>arr.length-1){
+        console.log("Bu indexde silmek ucun element yoxdur");
+    }
+    if(index===arr.length-1){
+        myNewArr.pop();
+    }
+    return myNewArr;
 }
-const result = myFunction(myArr, 3);
+const result = myFunction(myArr, 5);
 
+console.log(result);
 
 // Task-1
 
@@ -21,13 +28,20 @@ const myArr1 = [1, 3, 5, 7, 9];
 
 
 const myFunction1 = function (arr, index, value) {
-    const myNewArr=[];
+    const myNewArr = [];
+
     for (let i = 0; i < arr.length; i++) {
         if (i === index) {
             myNewArr.push(value)
         }
         myNewArr.push(arr[i]);
     }
-    console.log(myNewArr);
+    if (index > arr.length-1) {
+
+        myNewArr.push(value)
+    }
+    return myNewArr;
 }
-const result1=myFunction1(myArr1,2,`Salam Aleykum Brat`);
+const result1 = myFunction1(myArr1, 7, `Salam Aleykum Brat`);
+
+console.log(result1);
